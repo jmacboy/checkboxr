11908faeef14adc874ef888eb6c3e10acdcbc137
almarasc <almarasc@gmail.com>
2013-06-03 17:27:35 -0400

    Update checkboxr.js

--- js/checkboxr.js
+++ js/checkboxr.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function () {
+    // grouping function
     jQuery.fn.agrupar = function (opcions) {
+        //use the event 'change' to change the check
         $('#maestro').change(function () {
+            //takes the first element and question if it checked
             if ($("#maestro").is(':checked')) {
-                //cambia estado en caso de que este en otro falso
+                //changes state according to state true or false
                 $("INPUT[type='checkbox']").prop('checked', true);
             }
              else {
